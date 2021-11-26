48afa1832eda5db546d06b3d69ac77ebdde83d1a
Liu Geng <lerho@hotmail.com>
2021-11-26 13:42:39 +0800

    Fixed a bug in getting the Android version.

--- public/scripts/qrcode.js
+++ public/scripts/qrcode.js
@@ -162,13 +162,12 @@ var QRCode;
 		
 		if (/android/i.test(sAgent)) { // android
 			android = true;
-			var aMat = sAgent.toString().match(/android ([0-9]\.[0-9])/i);
+			var aMat = sAgent.toString().match(/android ([0-9]+\.?[0-9]?)/i);
 			
 			if (aMat && aMat[1]) {
 				android = parseFloat(aMat[1]);
 			}
 		}
-		
 		return android;
 	}
 	
